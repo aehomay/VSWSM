fd7026b6df7c35ec998a38676f738fedf0bbb218
aehomay <a.e.homay@gmail.com>
2020-04-10 21:37:14 +0200

    Version 1.1 Initialization Commit.

--- WindowsServiceManager/WindowsServiceManagerTest/Model/WindowsServiceInfoTest.cs
+++ WindowsServiceManager/WindowsServiceManagerTest/Model/WindowsServiceInfoTest.cs
@@ -14,22 +14,6 @@ namespace WindowsServiceManagerTest.Model
     [TestClass]
     public class WindowsServiceInfoTest
     {
-        /// <summary>
-        /// GetHashCode Compare Succssed
-        /// </summary>
-        [TestMethod]
-        public void GetHashCode_Compare_Succssed()
-        {
-            var info = new WindowsServiceInfo() { ServiceName = "Test1" };
-            Assert.AreEqual(info.GetHashCode(), info.ServiceName.GetHashCode());
-        }
-
-        [TestMethod]
-        public void Equals_Compare_Succssed()
-        {
-            var info_a = new WindowsServiceInfo() { ServiceName = "Test1", Status = System.ServiceProcess.ServiceControllerStatus.Running };
-            var info_b = new WindowsServiceInfo() { ServiceName = "Test1", Status = System.ServiceProcess.ServiceControllerStatus.Stopped };
-            Assert.IsTrue(info_a.Equals(info_b));
-        }
+       
     }
 }
